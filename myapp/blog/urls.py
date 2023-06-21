1846622a4f9f3af277fd4390586cf690e4d8b0fc
YuSeHyun <rexer0526@gamil.com>
2023-06-21 20:35:11 +0900

    second commit

--- myapp/blog/urls.py
+++ myapp/blog/urls.py
@@ -2,13 +2,17 @@ from django.urls import path
 from . import views
 
 # from blog.views import Index
+app_name = "blog"
 
 urlpatterns = [
     # path(pattern, mapping)
     # path("", views.index), #FBV
-    path("", views.Index.as_view())  # index page를 의미함
-    # 글 조회
+    # 글 목록 조회
+    path("", views.List.as_view(), name="list"),  # index page를 의미함
+    # 글 상세 조회
+    path("detail/<int:pk>/", views.Detail.as_view(), name="detail"),
     # 글 작성
+    path("write/", views.Write.as_view(), name="write"),
     # 글 수정
     # 글 삭제
     # 코멘트 작성
--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
+from django.views.generic import ListView, CreateView, DetailView
+from .models import Post
+from .forms import PostForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 # def index(request):
@@ -17,5 +21,46 @@ class Index(View):
         # return HttpResponse("Index page GET class")
 
         # DB에 접근해서 값을 가져와야 합니다.
+        # 게시판에 글들을 보여줘야해서 DA에서 "값 조회"
+        # MyModel.object.all()
+        post_objs = Post.objects.all()
         # cnotext = DB에서 가져온 값
-        return render(request, "blog/board.html")
+        context = {"posts": post_objs}
+        return render(request, "blog/board.html", context)
+
+
+# write
+# post - form
+# def write(request):
+#     if request.method == "POST":
+#         # form check
+#         form = PostForm(request.POST)
+#         if form.is_valid():
+#             post = form.save()
+#             return redirect("blog:list")
+
+#     else:
+#         form = PostForm()
+#         return render(request, "blog/write.html", {"form": form})
+
+
+# Django 자체의 class view 기능도 강력, 편리
+# model, template_name, context_object_name,
+# pagingate_by, form_class, form_valid(), get_queryset()
+# django.views.generic -> ListView
+class List(ListView):
+    model = Post  # model
+    template_name = "blog/post_list.html"  # template
+    context_object_name = "posts"  # 변수 값의 이름
+
+
+class Write(CreateView):
+    model = Post  # model
+    form_class = PostForm  # form
+    success_url = reverse_lazy("blog:list")  # active sucessing send url
+
+
+class Detail(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+    context_object_name = "post"
